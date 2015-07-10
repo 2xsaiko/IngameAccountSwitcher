4cb855812682cb58309a8127b84999549bbac058
The_Fireplace <the.f1repl4ce@gmail.com>
2015-07-09 21:14:48 -0500

    Updated version

--- src/main/java/com/github/mrebhan/ingameaccountswitcher/IngameAccountSwitcher.java
+++ src/main/java/com/github/mrebhan/ingameaccountswitcher/IngameAccountSwitcher.java
@@ -36,7 +36,7 @@ public class IngameAccountSwitcher {
 	public static IngameAccountSwitcher instance;
 	public static final String MODID = "IngameAccountSwitcher";
 	public static final String MODNAME = "In-game Account Switcher";
-	public static final String VERSION = "2.1.0.2";
+	public static final String VERSION = "2.1.0.3";
 	public static String releaseVersion = "";
 	public static String prereleaseVersion = "";
 	public static final String downloadURL = "http://goo.gl/1erpBM";
