b859e2f7b09388bda5a38007debe1eeb6343a65f
The_Fireplace <the.f1repl4ce@gmail.com>
2015-07-09 20:50:06 -0500

    Fixed gitignore

---
+++ build.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+TITLE The_Fireplace's Forge Tools - Build v1.1
+call gradlew build
+ECHO ****************************
+ECHO Building mod completed!
+ECHO ****************************
+PAUSE
