ca7fb3a7c834e85c8b84c41e93427c35b54665e0
The_Fireplace <the.f1repl4ce@gmail.com>
2015-07-09 23:52:01 -0500

    Work toward a better GUI

--- src/main/java/com/github/mrebhan/ingameaccountswitcher/gui/GuiAlts.java
+++ src/main/java/com/github/mrebhan/ingameaccountswitcher/gui/GuiAlts.java
@@ -11,6 +11,7 @@ import com.github.mrebhan.ingameaccountswitcher.tools.alt.AltDatabase;
 import com.github.mrebhan.ingameaccountswitcher.tools.alt.AltManager;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.StatCollector;
 /**
@@ -24,7 +25,11 @@ public class GuiAlts extends GuiScreen {
 
 	@Override
 	public void initGui() {
-
+		this.buttonList.add(new GuiButton(0, this.width / 2 + 4, this.height - 52, 160, 20, StatCollector.translateToLocal("ias.addaccount")));
+		this.buttonList.add(new GuiButton(1, this.width / 2 - 154 - 10, this.height - 52, 160, 20, StatCollector.translateToLocal("ias.login")));
+		this.buttonList.add(new GuiButton(2, this.width / 2 - 154 - 10, this.height - 28, 110, 20, StatCollector.translateToLocal("ias.login")+" "+StatCollector.translateToLocal("ias.offline")));
+		this.buttonList.add(new GuiButton(3, this.width / 2 + 4 + 50, this.height - 28, 110, 20, StatCollector.translateToLocal("gui.cancel")));
+		this.buttonList.add(new GuiButton(4, this.width / 2 - 50, this.height - 28, 100, 20, StatCollector.translateToLocal("ias.delete")));
 	}
 
 	@Override
@@ -53,25 +58,34 @@ public class GuiAlts extends GuiScreen {
 		if(!alts.isEmpty())
 			Tools.drawBorderedRect(0, selectedAlt * 12, fontRendererObj.getStringWidth(s) + 4, selectedAlt * 12 + 12, 1, 0xff444444, 0x00000000);
 		GL11.glPopMatrix();
-		this.drawCenteredString(fontRendererObj, "[UP,DOWN] to navigate, [ENTER] to login, [SHIFT+ENTER] for offline login, [+] to add, [-] to remove, [ESCAPE] for Main Menu", this.width / 2, this.height - 12, -1);
+		//this.drawCenteredString(fontRendererObj, "[UP,DOWN] to navigate, [ENTER] to login, [SHIFT+ENTER] for offline login, [+] to add, [-] to remove, [ESCAPE] for Main Menu", this.width / 2, this.height - 12, -1);
 		super.drawScreen(par1, par2, par3);
 	}
 
 	@Override
-	protected void keyTyped(char par1, int par2) {
-		if (par2 == Keyboard.KEY_ESCAPE) {
+	protected void actionPerformed(GuiButton button){
+		if(button.id == 3){
 			mc.displayGuiScreen(null);
-		}
-		if (par1 == '+') {
+		}else if(button.id == 0){
 			mc.displayGuiScreen(new GuiAddAlt());
-		}
-		if (par1 == '-') {
-			try {
-				AltDatabase.getInstance().getAlts().remove(selectedAlt);
-			} catch (Exception e) {
-				// TODO: handle exception
+		}else if(button.id == 4 && !alts.isEmpty()){
+			AltDatabase.getInstance().getAlts().remove(selectedAlt);
+		}else if(button.id == 1 && !alts.isEmpty()){
+			AccountData data = alts.get(selectedAlt);
+			failed = AltManager.getInstance().setUser(data.user, data.pass);
+			if (failed == null) {
+				Minecraft.getMinecraft().displayGuiScreen(null);
 			}
+		}else if(button.id == 2 && !alts.isEmpty()){
+			AccountData data = alts.get(selectedAlt);
+			AltManager.getInstance().setUserOffline(data.alias);
+			failed = null;
+			Minecraft.getMinecraft().displayGuiScreen(null);
 		}
+	}
+
+	@Override
+	protected void keyTyped(char par1, int par2) {
 		if (par2 == Keyboard.KEY_UP) {
 			if (selectedAlt > 0) {
 				selectedAlt--;
@@ -80,24 +94,6 @@ public class GuiAlts extends GuiScreen {
 		if (par2 == Keyboard.KEY_DOWN) {
 			if (selectedAlt < alts.size() - 1) {
 				selectedAlt++;
-			}
-		}
-		if (par2 == Keyboard.KEY_RETURN) {
-			if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
-				AccountData data = alts.get(selectedAlt);
-				AltManager.getInstance().setUserOffline(data.alias);
-				failed = null;
-				Minecraft.getMinecraft().displayGuiScreen(null);
-				return;
-			}
-			try {
-				AccountData data = alts.get(selectedAlt);
-				failed = AltManager.getInstance().setUser(data.user, data.pass);
-				if (failed == null) {
-					Minecraft.getMinecraft().displayGuiScreen(null);
-				}
-			} catch (Exception e) {
-				// TODO: handle exception
 			}
 		}
 	}
