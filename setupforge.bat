a1ea44347f0e563ce815cb956c3a3785f4fec27c
The_Fireplace <the.f1repl4ce@gmail.com>
2015-11-17 09:13:04 -0600

    Update Forge Setup tool

--- setupforge.bat
+++ setupforge.bat
@@ -1,8 +1,9 @@
 @ECHO OFF
-TITLE The_Fireplace's Forge Tools - Setup v1.3
+TITLE The_Fireplace's Forge Tools - Setup v1.4
 call gradlew setupDecompWorkspace
 call gradlew eclipse
+call gradlew idea
 ECHO ****************************
-ECHO Forge eclipse workspace setup completed!
+ECHO Forge eclipse and idea workspaces setup successfully!
 ECHO ****************************
 PAUSE
--- setupforge.sh
+++ setupforge.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 GRADLE_PATH="./gradlew"
-echo "The_Fireplace's Forge Tools - Setup v1.3"
+echo "The_Fireplace's Forge Tools - Setup v1.4"
 
 which gradle && GRADLE_PATH="$(which gradle)"
 
 "$GRADLE_PATH" setupDecompWorkspace
 "$GRADLE_PATH" eclipse
+"$GRADLE_PATH" idea
 echo "****************************"
-echo "Forge eclipse workspace setup completed!"
+echo "Forge eclipse and idea workspaces setup successfully!"
 echo "****************************"
 echo "Press any key to continue..."
 read -n 1 c
